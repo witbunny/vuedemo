d94b6d78878b619081dbd4f96cec4098bb7c8c23
witbunny <witbunny@163.com>
2022-05-04 15:30:10 +0800

    整理02.ref

---
+++ 02.src.ref/App.vue
@@ -0,0 +1,29 @@
+<template>
+	<div>
+		<School></School>
+		<School />
+		<School id="sch" ref="sch"></School>
+		<button ref="btn" @click="getRef">获取ref</button>
+	</div>
+</template>
+
+<script>
+	import School from "./components/School.vue";
+
+	export default {
+		name: "App",
+		components: {
+			School,
+		},
+		methods: {
+			getRef() {
+				console.log(document.getElementById("sch"));
+				console.log(this.$refs.btn);
+				console.log(this.$refs.sch);
+			}
+		}
+	};
+</script>
+
+<style>
+</style>
